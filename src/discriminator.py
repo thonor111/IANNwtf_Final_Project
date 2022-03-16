3d4879952218cdef46a48e6b2c12f86dc27904c1
thonor111 <t.nortmann@outlook.de>
2022-03-16 12:59:00 +0100

    adding the basic GAN structure as suggested by 'Adversarial Text Generation Without Reinforcement Learning'

--- src/discriminator.py
+++ src/discriminator.py
@@ -1,2 +1,33 @@
 import tensorflow as tf
 import tensorflow.keras as K
+
+
+class Discriminator(K.Model):
+
+    def __init__(self):
+        '''
+        Initializes the discriminator
+        '''
+        super(Discriminator, self).__init__()
+
+        self.input_layer = K.layers.Dense(100)
+        self.res_blocks = []
+        for i in range(40):
+            res_block = [
+                K.layers.Dense(100, activation='relu'),
+                K.layers.Dense(100),
+                K.layers.Add()
+            ]
+            self.res_blocks.append(res_block)
+
+        self.out = K.layers.Dense(1, activation='sigmoid')
+
+    @tf.function
+    def call(self, inputs, training):
+        x, inputs = self.input_layer(inputs, training=training)
+        for res_block in self.res_blocks:
+            x = res_block[0](x, training=training)
+            x = res_block[1](x, training=training)
+            x = res_block[2](x, inputs, training=training)
+        x = self.out(x, training=training)
+        return x
--- src/generator.py
+++ src/generator.py
@@ -1,2 +1,35 @@
 import tensorflow as tf
 import tensorflow.keras as K
+
+
+class Generator(K.Model):
+
+    def __init__(self):
+        '''
+        Initializes the generator
+        '''
+        super(Generator, self).__init__()
+
+        self.embedding_size = 100
+
+        self.input_layer = K.layers.Dense(100)
+        self.res_blocks = []
+        for i in range(40):
+            res_block = [
+                K.layers.Dense(100, activation='relu'),
+                K.layers.Dense(100),
+                K.layers.Add()
+            ]
+            self.res_blocks.append(res_block)
+
+        self.out = K.layers.Dense(self.embedding_size, activation='sigmoid')
+
+    @tf.function
+    def call(self, inputs, training):
+        x, inputs = self.input_layer(inputs, training=training)
+        for res_block in self.res_blocks:
+            x = res_block[0](x, training=training)
+            x = res_block[1](x, training=training)
+            x = res_block[2](x, inputs, training = training)
+        x = self.out(x, training=training)
+        return x
